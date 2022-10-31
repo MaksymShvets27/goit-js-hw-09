c0bbadef50647a06666d2621c829384d5f5c5106
Maksym Shvets <maxim27061993@outlook.com>
2022-10-31 18:22:47 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,3 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    promise.resolve()
-  } else {
-    promise.reject()
-  }
-}
-
 const formRef = document.querySelector("form");
 const inputsRef = document.querySelectorAll("input");
 const delayRef = inputsRef[0];
@@ -14,23 +5,33 @@ const stepRef = inputsRef[1];
 const amountRef = inputsRef[2];
 const btnRef = document.querySelector("button");
 
+function createPromise(position, delay) {
+  setTimeout(() => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      Promise.resolve({ position, delay });
+    } else {
+      Promise.reject({ position, delay });
+    }
+  }, delay);
+};
+
 formRef.addEventListener("input", () => {
-  const delayB = delayRef.value;
-  const step = stepRef.value;
-  const amount = amountRef.value;
+  const delay = parseInt(delayRef.value);
+  const step = parseInt(stepRef.value);
+  const amount = parseInt(amountRef.value);
 
   formRef.addEventListener("submit", (event) => {
     event.preventDefault();
-    for (i = 0; i <= amount; i++) {
-      createPromise(i + 1, delayB)
-        .then(({ position, delay }) => {
-          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
-      delayB += step;
+    for (i = 0; i <= amount; i += 1) {
+      console.log(delay, step, amount);
+      createPromise(i, delay).then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }).catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+      delay += step;
     };
-  })
+  });
 });
 
