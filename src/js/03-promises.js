a2816e2e072b4dd48bf2bc7976f7d7149afd6c20
Maksym Shvets <maxim27061993@outlook.com>
2022-11-01 10:19:42 +0100

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,4 @@
+import Notiflix from 'notiflix';
 const formRef = document.querySelector("form");
 const inputsRef = document.querySelectorAll("input");
 const delayRef = inputsRef[0];
@@ -6,32 +7,33 @@ const amountRef = inputsRef[2];
 const btnRef = document.querySelector("button");
 
 function createPromise(position, delay) {
-  setTimeout(() => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      return Promise.resolve({ position, delay });
-    } else {
-      return Promise.reject({ position, delay });
-    }
-  }, delay);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      };
+    }, delay);
+  });
 };
 
-formRef.addEventListener("input", () => {
-  const delay = parseInt(delayRef.value);
-  const step = parseInt(stepRef.value);
-  const amount = parseInt(amountRef.value);
-
-  formRef.addEventListener("submit", (event) => {
-    event.preventDefault();
-    for (let i = 0; i <= amount - 1; i += 1) {
-      console.log(delay, step, amount);
-      createPromise(i, delay).then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      }).catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-      delay += step;
-    };
-  });
+formRef.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let delay = parseInt(delayRef.value);
+  let step = parseInt(stepRef.value);
+  let amount = parseInt(amountRef.value);
+  let i = 1
+  for (i; i <= amount; i += 1) {
+    // console.log(delay, step, amount, i);
+    createPromise(i, delay).then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }).catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    delay += step;
+  };
 });
 
